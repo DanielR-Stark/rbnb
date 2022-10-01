83823874569e77e024ea0ecc58e7438bb247e53c
daniel rivera daniel.rivera0823@gmail.com <daniel.rivera0823@gmail.com>
2022-10-01 10:44:21 -0500

    Controller reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,50 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservation = Reservation.all
+  end
+
+  def show
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to @reservation
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to @reservation
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    redirect_to reservations_path
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:name)
+  end
+end
+
 end

819ff7379a98f38b0cdae80a3f5c2eb58da603f8
daniel rivera daniel.rivera0823@gmail.com <daniel.rivera0823@gmail.com>
2022-10-08 09:19:09 -0500

    Mis reservas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :lodgings do
-    resources :reservations, except: :index
+    resources :reservations, except: :index do
+      collection do
+        get "mis_reservas"
+      end
+    end
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
